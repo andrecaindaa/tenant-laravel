30b505cf2cbafaea2345fc6caff38ae93732df4e
andrecaindaa <andre.cainda@trainee.inovcorp.com>
2026-01-14 19:38:22 +0000

    feat: tenants model, migrations and user relationship

---
+++ app/Models/Tenant.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tenant extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'settings',
+    ];
+
+    protected $casts = [
+        'settings' => 'array',
+    ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withPivot('role')
+            ->withTimestamps();
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+
 
 class User extends Authenticatable
 {
@@ -45,4 +48,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function tenants(): BelongsToMany
+    {
+        return $this->belongsToMany(Tenant::class)
+            ->withPivot('role')
+            ->withTimestamps();
+    }
+
 }

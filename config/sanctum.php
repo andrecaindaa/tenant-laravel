fd4d298d453e1dc15f9fd54f9d64809d5bde49f4
andrecaindaa <andre.cainda@trainee.inovcorp.com>
2026-01-20 22:18:54 +0000

    feat(auth): configuração de autenticação com Sanctum e CORS

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,12 +15,17 @@ return [
     |
     */
 
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS',
+    'localhost,localhost:3000,localhost:5173,127.0.0.1,127.0.0.1:8000,::1,tenant-project.test'
+)),
+
+/*
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
-    ))),
+    ))),*/
 
     /*
     |--------------------------------------------------------------------------
